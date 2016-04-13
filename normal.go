65466d61f7d990872f68fbad9da84d63c9da4e12
Eric Burel <e.burel.contact@gmail.com>
2016-04-13 19:03:01 +0200

    Implemented R and Rn functions for Normal distribution

--- normal.go
+++ normal.go
@@ -1,5 +1,7 @@
 package rgo
-
+import(
+    "math/rand"
+)
 // Normal A variable following a normal distribution
 type Normal struct{
     mu float64
@@ -16,5 +18,19 @@ func NewNormal(mu float64, sigma float64) (x *Normal){
 // (mu=0, sigma=1)
 func NewStdNormal() (x *Normal){
     x = &Normal{0,1}
+    return
+}
+
+// R Generate a random value following the same Normal distribution as x
+func (x Normal) R() (res float64){
+    res = rand.NormFloat64() * x.sigma + x.mu
+    return
+}
+// Rn Generate an array of random values following the same Normal distribution as x
+func (x Normal) Rn(n int) (res []float64){
+    res = make([]float64, n)
+    for i := 0; i < n; i++ {
+        res[i] = rand.NormFloat64() * x.sigma + x.mu
+    }
     return
 }
